a3e1d32acc0e479e3ddd544c9b8a248ce69943a2
ubaidnaveed <ubaidub97@gmail.com>
2024-06-06 06:38:16 +0300

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchemea = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    
+},{timestamps:true} 
+);
+
+const User = mongoose.model('User',userSchemea);
+
+export default User;
