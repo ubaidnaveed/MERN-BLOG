8f7589aa96237625c5520c2d651556d6479084db
ubaidnaveed <ubaidub97@gmail.com>
2024-06-11 10:32:27 +0500

    add functionality to sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
+  const navigate = useNavigate()
+  const [formData,setFormData]= useState({})
+  const [errorMessage,setErrorMessage] = useState(null)
+  const [loading,setLoading] = useState(false)
+  const handleChange = (e) =>{
+    setFormData({...formData,[e.target.id]:e.target.value.trim()})
+  }
+
+  const handleSumbit = async(e) =>{
+    e.preventDefault()
+    if(!formData.username || !formData.password || !formData.email){
+      return setErrorMessage("All fields are required")
+      
+    }
+    try {
+      setLoading(true)
+      setErrorMessage(null)
+      const res = await fetch ('api/auth/signup',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(formData)
+      })
+      const data = await res.json()
+      if(data.success===false){
+        setLoading(false)
+        return setErrorMessage(data.message)
+      }else{
+        setLoading(false)
+        
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setLoading(false)
+    }
+  }
   return (
     <div className='min-h-screen mt-20 mr-1 ml-1'>
       <div className='flex max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -20,11 +58,11 @@ export default function SignUp() {
           This is my practice project. U can sign-up using email an password or with google. 
         </p>
         </div>
-        <div flex-1>
-          <form className='flex flex-col gap-4'>
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSumbit}>
             <div className='flex flex-col '>
             <label className='font-semibold'>Username</label>
-            <input 
+            <input onChange={handleChange}
             className='rounded-lg shadow-lg'
             type='text'
             placeholder='Username'
@@ -32,22 +70,22 @@ export default function SignUp() {
             </div>
             <div className='flex flex-col'>
             <label className='font-semibold'>Email</label>
-            <input 
+            <input onChange={handleChange}
             className='rounded-lg shadow-lg'
-            type='text'
+            type='email'
             placeholder='name@company.com'
             id='email'/>
             </div>
             <div className='flex flex-col'>
             <label className='font-semibold'>Password</label>
-            <input 
+            <input onChange={handleChange}
             className='rounded-lg shadow-lg'
-            type='text'
+            type='password'
             placeholder='Password'
             id='password'/>
             </div>
-            <button className='w-80 h-9 shadow-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 font-semibold text-white rounded-lg'>
-              Sign Up
+            <button disabled={loading} className='w-80 h-9 shadow-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 font-semibold text-white rounded-lg'>
+              {loading? 'Loading...':'Sign Up'}
             </button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
@@ -56,6 +94,9 @@ export default function SignUp() {
           Sign In
           </Link>
           </div>
+          {errorMessage && (
+            <p className='text-red-500 text-sm'>{errorMessage}</p>
+          )}
           </div>
        
      
